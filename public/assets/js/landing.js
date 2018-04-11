49f57af27e8179d6804ad59a7ae354f4891e66ae
ebweinberger <elibweinberger@gmail.com>
2018-04-11 00:35:32 -0400

    finished modals for login and signup

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -8,13 +8,14 @@ loginBtn.onclick = function() {
   loginModal.style.display = "block";
 }
 
+signupBtn.onclick = function() {
+  signupModal.style.display = "block";
+}
+
+
 window.onclick = function(event) {
   if (event.target == loginModal || event.target == signupModal){
     signupModal.style.display = "none";
     loginModal.style.display = "none";
   }
-}
-
-signupBtn.onclick = function() {
-  signupModal.style.display = "block";
 }
