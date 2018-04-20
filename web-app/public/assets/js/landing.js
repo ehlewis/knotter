ae9531279735113e92a8e9d6581ebfa94b4920b7
ebweinberger <elibweinberger@gmail.com>
2018-04-20 00:24:22 -0400

    honestly dont even remember all the things I did in this one. Oops.

--- web-app/public/assets/js/landing.js
+++ web-app/public/assets/js/landing.js
@@ -4,6 +4,9 @@ var loginModal = document.getElementById('login-modal');
 var signupBtn = document.getElementById('sign-up-button');
 var signupModal = document.getElementById('sign-up-modal');
 
+var alreadyLoginBtn = document.getElementById('already-login');
+var alreadySignupBtn = document.getElementById('already-sign-up');
+
 loginBtn.onclick = function() {
   loginModal.style.display = "block";
 }
@@ -12,10 +15,35 @@ signupBtn.onclick = function() {
   signupModal.style.display = "block";
 }
 
+alreadyLoginBtn.onclick = function() {
+  signupModal.style.display = "none";
+  loginModal.style.display = "block";
+  document.getElementById("login-name").value = "";
+  document.getElementById("login-pass").value = "";
+  document.getElementById("sign-up-email").value = "";
+  document.getElementById("sign-up-name").value = "";
+  document.getElementById("sign-up-pass").value = "";
+}
+
+alreadySignupBtn.onclick = function() {
+  loginModal.style.display = "none";
+  signupModal.style.display = "block";
+  document.getElementById("login-name").value = "";
+  document.getElementById("login-pass").value = "";
+  document.getElementById("sign-up-email").value = "";
+  document.getElementById("sign-up-name").value = "";
+  document.getElementById("sign-up-pass").value = "";
+}
+
 
 window.onclick = function(event) {
   if (event.target == loginModal || event.target == signupModal){
     signupModal.style.display = "none";
     loginModal.style.display = "none";
+    document.getElementById("login-name").value = "";
+    document.getElementById("login-pass").value = "";
+    document.getElementById("sign-up-email").value = "";
+    document.getElementById("sign-up-name").value = "";
+    document.getElementById("sign-up-pass").value = "";
   }
 }
