69381fb03bbebc800b0a938403d473249c97477a
ebweinberger <elibweinberger@gmail.com>
2018-04-21 23:47:36 -0400

    signup form validation now working properly

--- web-app/public/assets/js/landing.js
+++ web-app/public/assets/js/landing.js
@@ -7,6 +7,59 @@ var signupModal = document.getElementById('sign-up-modal');
 var alreadyLoginBtn = document.getElementById('already-login');
 var alreadySignupBtn = document.getElementById('already-sign-up');
 
+var signupName = document.getElementById('sign-up-name');
+var signupPass = document.getElementById('sign-up-pass');
+var signupEmail = document.getElementById('sign-up-email');
+var loginForm = document.getElementById('login-form');
+var signupForm = document.getElementById('sign-up-form');
+
+var createButton = document.getElementById('create-button')
+
+function enableButton(){
+  var reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var rePassword = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+  var reName = /^[a-z]+$/;
+
+  if(reName.test(String(signupName.value).toLowerCase()) && reEmail.test(String(signupEmail.value).toLowerCase()) && rePassword.test(String(signupPass.value).toLowerCase())){
+    createButton.disabled = false;
+  }
+}
+
+function validateName() {
+    var re = /^[a-z]+$/;
+    if (!(re.test(String(signupName.value).toLowerCase()))){
+      signupName.style.border = "1px solid red";
+    }
+    else if(re.test(String(signupName.value).toLowerCase())){
+      signupName.style.border = "0px";
+    }
+    enableButton();
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!(re.test(String(signupEmail.value).toLowerCase()))){
+      signupEmail.style.border = "1px solid red";
+    }
+    else if(re.test(String(signupEmail.value).toLowerCase())){
+      signupEmail.style.border = "0px";
+    }
+    enableButton();
+}
+
+function validatePassword() {
+    var re = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    if (!(re.test(String(signupPass.value).toLowerCase()))){
+      signupPass.style.border = "1px solid red";
+    }
+    else if(re.test(String(signupPass.value).toLowerCase())){
+      signupPass.style.border = "0px";
+    }
+    enableButton();
+}
+
+
+
 loginBtn.onclick = function() {
   loginModal.style.display = "block";
 }
@@ -45,5 +98,8 @@ window.onclick = function(event) {
     document.getElementById("sign-up-email").value = "";
     document.getElementById("sign-up-name").value = "";
     document.getElementById("sign-up-pass").value = "";
+    signupEmail.style.border = "0px";
+    signupName.style.border = "0px";
+    signupPass.style.border = "0px";
   }
 }
--- web-app/server.js
+++ web-app/server.js
@@ -298,14 +298,14 @@ app.get('/logout', function(req, res) {
 });*/
 
 app.post('/signup', passport.authenticate('local-signup', {
-successRedirect : '/profile', // redirect to the secure profile section
+successRedirect : '/dashboard', // redirect to the secure profile section
 failureRedirect : '/signup', // redirect back to the signup page if there is an error
 failureFlash : true // allow flash messages
 }));
 
 // process the login form
 app.post('/login', passport.authenticate('local-login', {
-   successRedirect : '/profile', // redirect to the secure profile section
+   successRedirect : '/dashboard', // redirect to the secure profile section
    failureRedirect : '/login', // redirect back to the signup page if there is an error
    failureFlash : true // allow flash messages
 }));
