625ccc26c7ece8ed8159c0c64ed4cb6d9c1e186c
Evan Lewis <ehlewis@me.com>
2018-04-22 22:52:58 -0400

    changing json calls to api

--- web-app/server.js
+++ web-app/server.js
@@ -338,6 +338,15 @@ app.get('/name', function(request, response, next) {
 });
 
 
+app.get('/nvm', isLoggedIn, function(request, response, next) {
+  response.render('nvm.ejs', {
+    PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+    PLAID_ENV: PLAID_ENV,
+  });
+});
+
+
+
 app.post('/name', function(req, res, next) {
 
     /* var product = {  name: "A" };
