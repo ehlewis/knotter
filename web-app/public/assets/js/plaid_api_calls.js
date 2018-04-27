205b9f52847f1e66fe0170aa96d9f3b6db89f4e0
Evan Lewis <ehlewis@me.com>
2018-04-27 00:38:50 -0400

    Graph is now dynamically loaded

--- web-app/public/assets/js/plaid_api_calls.js
+++ web-app/public/assets/js/plaid_api_calls.js
@@ -3,6 +3,8 @@ var transaction_array = [];
 var all_transactions = [];
 var account_array = [];
 var transactions_loaded = false;
+var graphDataLoaded = false;
+var graphData = [0,0,0,0,0,0,0,0,0,0,0,0];
 
 
 function getNumAccts() {
@@ -124,9 +126,107 @@ function transactionArraytoCSV() {
                all_transactions.push(transaction_array[i].transactions[j]);
            }
        }
-       //console.log(all_transactions);
+       console.log(all_transactions);
    }
 }
 
+function transactionCSVtoFrequencyGraph() {
+   if (transactions_loaded == true) {
+       var transDate = new Date();
+       var temp = -1;
+       for(var i = 0; i < transaction_array.length; i++){
+           for (var j = 0; j < transaction_array[i].transactions.length; j++) {
+               transDate = new Date(transaction_array[i].transactions[j].date);
+               //console.log(transDate.getMonth());
+               temp = transDate.getMonth();
+                if(temp == 0){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 1){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 2){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 3){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 4){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 5){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 6){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 7){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 8){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 9){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 10){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+                else if(temp == 11){
+                    graphData[temp] = graphData[temp] + 1;
+                }
+           }
+       }
+       console.log(graphData);
+       graphDataLoaded = true;
+       //return graphData;
+   }
+}
 
 //-----------------------------------------------------
+
+//------------------misc
+
+function account_id_to_name(in_id) {
+    //ported but untested
+    for (var i = 0; i < accounts_array.length; i++) {
+        if(accounts_array[i].account_id === in_id){
+            console.log(acct.name);
+            return acct.name;
+        }
+    }
+}
+
+
+
+function makeChart(){
+    if (graphDataLoaded == true) {
+        console.log("Making graph " + graphData);
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+            // The data for our dataset
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                datasets: [{
+                    backgroundColor: '#00b09b', /*rgb(255, 99, 132)*/
+                    borderColor: '#00b09b', /*rgb(255, 99, 132)*/
+                    data: graphData,
+                }]
+            },
+
+            // Configuration options go here
+            options: {
+                legend: {
+                    display: false,
+                    labels: {
+                        fontColor: 'rgb(255, 99, 132)'
+                    }
+                },
+                responsive: false
+            }
+        });
+    }
+}
+
+//------------------------
