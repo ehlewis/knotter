e8c93956058e19b83879220dc741f01e1a078ffa
Evan Lewis <ehlewis@me.com>
2018-09-14 14:01:37 -0400

    users cant go back without resub

--- web-app/server.js
+++ web-app/server.js
@@ -97,6 +97,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(function(request, response, next) {
+  response.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
+
 
 //=====GETS=====
 
