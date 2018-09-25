bac7e01f9d7a7a003e3ee703c50ade6cc30aa4cf
Evan Lewis <ehlewis@me.com>
2018-09-25 15:59:38 -0400

    Promisfied refresh_cahce

--- app/routes/cache_functions.js
+++ app/routes/cache_functions.js
@@ -1,4 +1,6 @@
 var link_functions = require('./link_functions');
+var logger = require('../config/logger');
+var BPromise = require('bluebird');
 
 module.exports = {
     //Deletes existing keys and replaces them, does NOT fail if they keys don't exist
@@ -7,16 +9,33 @@ module.exports = {
         redis_client.del(request.user._id.toString() + "accounts");
         redis_client.del(request.user._id.toString() + "item");
         redis_client.del(request.user._id.toString() + "transactions");
+        logger.debug(request.user._id + " Cache was emptied");
         cache_user_data(request, response, next);
     },
 
 };
 
 // Helper function that caches all the users data
-function cache_user_data(request, response, next) {
-    num = request.user.items.length; //get num items to iterate through
-    link_functions.cache_user_institutions(request, response, next, num);
-    link_functions.cache_user_accounts(request, response, next, num);
-    link_functions.cache_items(request, response, next, num);
-    link_functions.cache_transactions(request, response, next, num);
+async function cache_user_data(request, response, next) {
+    return new Promise(function (resolve, reject) {
+        var myPromises = [];
+        num = request.user.items.length; //get num items to iterate through
+        myPromises.push(link_functions.cache_user_institutions(request, response, num, function(){
+            return;
+        }));
+        myPromises.push(link_functions.cache_user_accounts(request, response, num, function(){
+            return;
+        }));
+        //myPromises.push(link_functions.cache_items(request, response, next, num));
+        myPromises.push(link_functions.cache_transactions(request, response, num, function(){
+            return;
+        }));
+
+        BPromise.all(myPromises).then(function() {
+            //logger.debug(myPromises);
+            next();
+            //resolve();
+            logger.debug(request.user._id + " Cache was populated");
+        });
+    });
 }
