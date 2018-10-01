c15af3ad5f76cc276d05deb6e1a4cc2c8d720b0a
Evan Lewis <ehlewis@me.com>
2018-10-01 18:59:26 -0400

    added flags

--- app/config/mongo_setup.js
+++ app/config/mongo_setup.js
@@ -3,12 +3,22 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //var User = mongoose.model('User');
-var mongo_url = "mongodb://localhost:27017/link"; // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 //var configDB = require('./database.js');
 var collection = null;
 var logger = require('./logger.js');
 
 mongoose.Promise = require('bluebird');
+if (SERVICE_CONNECTION === "local-sandbox"){
+    var mongo_url = "mongodb://localhost:27017/link"; // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+}
+else if (SERVICE_CONNECTION === "remote-staging"){
+    //var mongo_url = "mongodb://link_server:link_server@linkcluster0-shard-00-00-97dei.gcp.mongodb.net/link";
+    var mongo_url = "mongodb://localhost:27017/link"; // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+}
+if (SERVICE_CONNECTION === "production"){
+    var mongo_url = "mongodb://localhost:27017/link";
+}
 
 // ==Configuration*==
 module.exports = function() {
