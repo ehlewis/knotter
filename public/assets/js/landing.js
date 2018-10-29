27f4361c1213f266736f6f1f24f361000721e1c4
ebweinberger <ebweinberger@gmail.com>
2018-10-28 22:38:31 -0400

    huge js revamp

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,22 +1,14 @@
+var modal = document.getElementById('modal');
+
 var loginBtn = document.getElementById('login-button');
-var loginModal = document.getElementById('login-modal');
-
 var signupBtn = document.getElementById('sign-up-button');
-var signupModal = document.getElementById('sign-up-modal');
 
-var alreadyLoginBtn = document.getElementById('already-login');
-var alreadySignupBtn = document.getElementById('already-sign-up');
+var email = document.getElementById('email');
+var pass = document.getElementById('pass');
 
-var signupName = document.getElementById('sign-up-name');
-var signupPass = document.getElementById('sign-up-pass');
-var signupEmail = document.getElementById('sign-up-email');
-var loginName = document.getElementById('login-name');
-var loginPass = document.getElementById('login-pass');
-var loginForm = document.getElementById('login-form');
-var signupForm = document.getElementById('sign-up-form');
+var loginSubmit = document.getElementById('loginSubmit');
+var signupSubmit = document.getElementById('signupSubmit');
 
-var createButton = document.getElementById('create-button');
-var showPassButton = document.getElementById('showbtn');
 
 var hideIcon = document.getElementById('hide-icon');
 
@@ -33,48 +25,33 @@ function showPass(){
 
 
 loginBtn.onclick = function() {
-  loginModal.style.display = "block";
+  modal.style.display = "block";
+  loginSubmit.style.display = "block";
 }
 
 signupBtn.onclick = function() {
-  signupModal.style.display = "block";
-}
-
-alreadyLoginBtn.onclick = function() {
-  signupModal.style.display = "none";
-  loginModal.style.display = "block";
-  document.getElementById("login-name").value = "";
-  document.getElementById("login-pass").value = "";
-  document.getElementById("sign-up-email").value = "";
-  document.getElementById("sign-up-name").value = "";
-  document.getElementById("sign-up-pass").value = "";
-}
-
-alreadySignupBtn.onclick = function() {
-  loginModal.style.display = "none";
-  signupModal.style.display = "block";
-  document.getElementById("login-name").value = "";
-  document.getElementById("login-pass").value = "";
-  document.getElementById("sign-up-email").value = "";
-  document.getElementById("sign-up-name").value = "";
-  document.getElementById("sign-up-pass").value = "";
+  modal.style.display = "block";
+  signupSubmit.style.display = "block";
 }
 
 
 window.onclick = function(event) {
-  if (event.target == loginModal || event.target == signupModal){
-    signupModal.style.display = "none";
-    loginModal.style.display = "none";
-    document.getElementById("login-name").value = "";
-    document.getElementById("login-pass").value = "";
-    document.getElementById("sign-up-email").value = "";
-    document.getElementById("sign-up-name").value = "";
-    document.getElementById("sign-up-pass").value = "";
-    signupEmail.style.border = "0px";
-    signupName.style.border = "0px";
-    signupPass.style.border = "0px";
-    loginName.style.border = "0px";
-    loginPass.style.border = "0px";
+  if (event.target == modal){
+    modal.style.display = "none";
+    loginSubmit.style.display = "none";
+    signupSubmit.style.display = "none";
+    email.value = "";
+    pass.value = "";
+    // document.getElementById("login-name").value = "";
+    // document.getElementById("login-pass").value = "";
+    // document.getElementById("sign-up-email").value = "";
+    // document.getElementById("sign-up-name").value = "";
+    // document.getElementById("sign-up-pass").value = "";
+    // signupEmail.style.border = "0px";
+    // signupName.style.border = "0px";
+    // signupPass.style.border = "0px";
+    // loginName.style.border = "0px";
+    // loginPass.style.border = "0px";
   }
 }
 
