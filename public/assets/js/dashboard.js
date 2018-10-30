1a83d5f95e7d075bff4302fa02e9d3f660d1eff9
Evan Lewis <ehlewis@me.com>
2018-10-30 16:15:28 -0400

    getUserTransactions cleaning and commenting

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,78 +1,124 @@
-function displayMenu(){
-  var menu = document.getElementById('menu');
-  if(menu.style.display == "none"){
-    menu.style.display = "block";
-  }else{
-    menu.style.display = "none";
-  }
+function displayMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 }
 
-function getUserDataFromCache() {
-  $.getJSON("api/user_data", function(data) {
-    // Make sure the data contains the username as expected before using it
-    console.log(data.user);
-    if (data.user.items.length != 0) {
-      document.getElementById('container_block').innerHTML = ''
-      $.get('/api/get_cached_transactions').success(
-        function(success) {
-          if (success != null) {
-            addCards(success);
-            addTransactions(success);
-          } else {
-            console.log("Im null and need to refresh");
-            $.get('/api/refresh_cache').success(function(refresh_success) {
-              console.log("I'm running");
-              $.get('/api/get_cached_transactions').success(
-                function(success_transactions) {
-                  if (success_transactions != null) {
-                    addCards(success_transactions);
-                    addTransactions(success_transactions);
-                  }
-                  console.log(success_transactions);
+function getUserTransactions() {
+    //Gets users transactions and handles if the cache is empty by refreshing it
+    $.getJSON("/api/user_data", function(data) {
+        // Make sure the data contains the data as expected before using it
+        if (data.user.items.length != 0) {
+            //If there are items linked
+            $.get('/api/get_cached_transactions').success(function(success) {
+                if (success != null) {
+                    return success;
+                } else {
+                    //If there are items but nothing in the cache
+                    $.get('/api/refresh_cache').success(function(refresh_success) {
+                        //Cache refreshes
+                        $.get('/api/get_cached_transactions').success
+                            //We try again
+                            function(success_transactions) {
+                                if (success_transactions != null) {
+                                    return success_transactions;
+                                }
+                                else{
+                                    cosnole.log("Error: Could not get transactions after cache refresh");
+                                }
+                            }).error(
+                            function(error) {
+                                console.log(error)
+                            });
+                    });
+                }
+            }).error(
+                function(error) {
+                    console.log(error);
+                });
+        } else {
+            //Runs if there are no items linked
+            console.log("Loaded nothing linked");
+            document.getElementById('container_block').innerHTML = 'Link an account by pressing the + below'
+        }
+    });
+}
+
+function getUser() {
+    $.getJSON("api/user_data", function(data) {
+        // Make sure the data contains the username as expected before using it
+        return data;
+    });
+}
+
+function createInstitutionDivs() {
+    $.getJSON("api/user_data", function(data) {
+        // Make sure the data contains the username as expected before using it
+        console.log(data.user);
+        if (data.user.items.length != 0) {
+            document.getElementById('container_block').innerHTML = ''
+            $.get('/api/get_cached_transactions').success(
+                function(success) {
+                    if (success != null) {
+                        addCards(success);
+                        addTransactions(success);
+                    } else {
+                        console.log("Im null and need to refresh");
+                        $.get('/api/refresh_cache').success(function(refresh_success) {
+                            console.log("I'm running");
+                            $.get('/api/get_cached_transactions').success(
+                                function(success_transactions) {
+                                    if (success_transactions != null) {
+                                        addCards(success_transactions);
+                                        addTransactions(success_transactions);
+                                    }
+                                    console.log(success_transactions);
+                                }).error(
+                                function(error) {
+                                    console.log(error)
+                                });
+                        });
+                    }
+                    console.log(success);
                 }).error(
                 function(error) {
-                  console.log(error)
+                    console.log(error);
                 });
-            });
-          }
-          console.log(success);
-        }).error(
-        function(error) {
-          console.log(error);
-        });
-    } else {
-      console.log("Loaded");
-      document.getElementById('container_block').innerHTML = 'Link an account by pressing the + below'
-      //Tell Grey thingy that nothing is going to load or render it idk
-    }
-  });
+        } else {
+            console.log("Loaded");
+            document.getElementById('container_block').innerHTML = 'Link an account by pressing the + below'
+        }
+    });
 }
 
 
 
 function addCards(user_institutions) {
-  var current_cards = ['ins_1', 'ins_2', 'ins_3', 'ins_4', 'ins_5', 'ins_6', 'ins_7', 'ins_9', 'ins_10', 'ins_11', 'ins_13', 'ins_14', 'ins_15', 'ins_16', 'ins_19', 'ins_20', 'ins_21', 'ins_23', 'ins_24', 'ins_27', 'ins_29']
-  for (var insitution = 0; insitution < user_institutions.length; insitution++) {
-    var ins_id = user_institutions[insitution].item.institution_id;
-    //Group by insitution
+    var current_cards = ['ins_1', 'ins_2', 'ins_3', 'ins_4', 'ins_5', 'ins_6', 'ins_7', 'ins_9', 'ins_10', 'ins_11', 'ins_13', 'ins_14', 'ins_15', 'ins_16', 'ins_19', 'ins_20', 'ins_21', 'ins_23', 'ins_24', 'ins_27', 'ins_29']
+    for (var insitution = 0; insitution < user_institutions.length; insitution++) {
+        var ins_id = user_institutions[insitution].item.institution_id;
+        //Group by insitution
 
-    var new_subsection = document.createElement('div');
-    new_subsection.className = "ins-subsection";
-    new_subsection.id = ins_id + "subsection";
-    new_subsection.innerHTML = '<button class="show" id="showbtn' + ins_id + '"onclick="expandSubsection(this.parentElement.id, this.id, hidebtn'+ins_id+'.id)"> Show More </button>\
-    <button class="hide" id = "hidebtn' + ins_id + '" onclick="collapseSubsection(this.parentElement.id, showbtn'+ins_id+'.id, this.id)"> Show Less </button>';
+        var new_subsection = document.createElement('div');
+        new_subsection.className = "ins-subsection";
+        new_subsection.id = ins_id + "subsection";
+        new_subsection.innerHTML = '<button class="show" id="showbtn' + ins_id + '"onclick="expandSubsection(this.parentElement.id, this.id, hidebtn' + ins_id + '.id)"> Show More </button>\
+    <button class="hide" id = "hidebtn' + ins_id + '" onclick="collapseSubsection(this.parentElement.id, showbtn' + ins_id + '.id, this.id)"> Show Less </button>';
 
-    document.getElementById('container_block').appendChild(new_subsection);
+        document.getElementById('container_block').appendChild(new_subsection);
 
-    for (var account = 0; account < user_institutions[insitution].accounts.length; account++) {
-      if ($.inArray(user_institutions[insitution].item.institution_id, current_cards) == -1) {
-        var ins = 'default_card';
-      } else {
-        var ins = user_institutions[insitution].item.institution_id;
-      }
-      var new_card = document.createElement('div');
-      new_card.className = "content";
-      new_card.innerHTML = '<div class="card">\
+        for (var account = 0; account < user_institutions[insitution].accounts.length; account++) {
+            if ($.inArray(user_institutions[insitution].item.institution_id, current_cards) == -1) {
+                var ins = 'default_card';
+            } else {
+                var ins = user_institutions[insitution].item.institution_id;
+            }
+            var new_card = document.createElement('div');
+            new_card.className = "content";
+            new_card.innerHTML = '<div class="card">\
         <div class="card-content" onClick="createMoreInfoModal(this.id)" id="' + user_institutions[insitution].accounts[account].account_id + '">\
           <div class="bank-logo">\
             <img id="img' + user_institutions[insitution].accounts[account].account_id + '" src="assets/cardLogos/' + ins + '.svg">\
@@ -98,39 +144,39 @@ function addCards(user_institutions) {
           </div>\
         </div>';
 
-      document.getElementById(ins_id + "subsection").appendChild(new_card);
+            document.getElementById(ins_id + "subsection").appendChild(new_card);
+        }
     }
-  }
 }
 
-function addTransactions(user_institutions) {
-  for (insitution = 0; insitution < user_institutions.length; insitution++) {
-    for (transaction = 0; transaction < user_institutions[insitution].transactions.length; transaction++) {
-      var new_transaction = document.createElement('tr');
-      new_transaction.innerHTML = '<td>' + user_institutions[insitution].transactions[transaction].date + '</td>\
+function addTransactionsToCards(user_institutions) {
+    for (insitution = 0; insitution < user_institutions.length; insitution++) {
+        for (transaction = 0; transaction < user_institutions[insitution].transactions.length; transaction++) {
+            var new_transaction = document.createElement('tr');
+            new_transaction.innerHTML = '<td>' + user_institutions[insitution].transactions[transaction].date + '</td>\
               <td>' + user_institutions[insitution].transactions[transaction].name + '</td>\
               <td>$' + user_institutions[insitution].transactions[transaction].amount + '</td>'
-      document.getElementById('table' + user_institutions[insitution].transactions[transaction].account_id).appendChild(new_transaction);
+            document.getElementById('table' + user_institutions[insitution].transactions[transaction].account_id).appendChild(new_transaction);
+        }
     }
-  }
 }
 
 function expandSubsection(subsection_id, showbtn_id, hidebtn_id) {
-  var subsection = document.getElementById(subsection_id);
-  var showbtn = document.getElementById(showbtn_id);
-  var hidebtn = document.getElementById(hidebtn_id)
-  subsection.style.maxHeight = "100%";
-  showbtn.style.display = "none";
-  hidebtn.style.display = "block";
-  subsection.className = "ins-subsection nofade";
+    var subsection = document.getElementById(subsection_id);
+    var showbtn = document.getElementById(showbtn_id);
+    var hidebtn = document.getElementById(hidebtn_id)
+    subsection.style.maxHeight = "100%";
+    showbtn.style.display = "none";
+    hidebtn.style.display = "block";
+    subsection.className = "ins-subsection nofade";
 }
 
 function collapseSubsection(subsection_id, showbtn_id, hidebtn_id) {
-  var subsection = document.getElementById(subsection_id);
-  var showbtn = document.getElementById(showbtn_id);
-  var hidebtn = document.getElementById(hidebtn_id)
-  subsection.style.maxHeight = "400px";
-  showbtn.style.display = "block";
-  hidebtn.style.display = "none";
-  subsection.className = "ins-subsection";
+    var subsection = document.getElementById(subsection_id);
+    var showbtn = document.getElementById(showbtn_id);
+    var hidebtn = document.getElementById(hidebtn_id)
+    subsection.style.maxHeight = "400px";
+    showbtn.style.display = "block";
+    hidebtn.style.display = "none";
+    subsection.className = "ins-subsection";
 }
