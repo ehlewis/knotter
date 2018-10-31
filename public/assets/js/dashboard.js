e8e5d4889e739b22b1b0bbbefcb7f64d9b90ba43
ebweinberger <ebweinberger@gmail.com>
2018-10-30 21:43:58 -0400

    display block

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,11 +1,3 @@
-function displayMenu() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-});
 
 
 function displayMenu(){
