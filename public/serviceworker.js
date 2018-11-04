96a4a8050a355a27d4b6845d9ed50d35ba864952
ebweinberger <ebweinberger@gmail.com>
2018-11-04 18:02:14 -0500

    sw?

---
+++ public/serviceworker.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'knotter-sw-cache';
+var urlsToCache = [
+  '/views/landing.ejs',
+  '/public/assets/css/boilerplate.css',
+  '/public/assets/css/landing.css',
+  '/public/assets/js/landing.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
