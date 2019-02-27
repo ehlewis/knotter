783f1aeab5ce25cac2230dc97226407be87464a0
Evan Lewis <ehlewis@me.com>
2019-02-27 16:05:30 -0500

    fixing a logger issue with AWS

--- app/config/logger.js
+++ app/config/logger.js
@@ -22,7 +22,7 @@ if(process.env.SERVICE_CONNECTION==="local-sandbox"){
 
 //STAGING setup
 else if(process.env.SERVICE_CONNECTION==="remote-sandbox" || process.env.SERVICE_CONNECTION === "remote-dev"){
-    const {LoggingWinston} = require('@google-cloud/logging-winston');
+    /*const {LoggingWinston} = require('@google-cloud/logging-winston');
     const loggingWinston = new LoggingWinston();
 
     var logger = winston.createLogger({
@@ -39,6 +39,21 @@ else if(process.env.SERVICE_CONNECTION==="remote-sandbox" || process.env.SERVICE
             }),
             new winston.transports.File({filename: 'link.log'}),
             loggingWinston
+        ]
+    });*/
+    var logger = winston.createLogger({
+        level: 'debug',
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.printf(info => {
+                return `${info.timestamp} ${info.level}: ${info.message}`;
+            })
+        ),
+        transports: [
+            new winston.transports.Console({
+                format: winston.format.combine(winston.format.colorize(),winston.format.simple())
+            }),
+            new winston.transports.File({filename: 'link.log'})
         ]
     });
 }
